d9af7bef5c8f52b3c68e1ced23e8339d7ff7d8b2
Agnieszka Markiewicz <agnieszka.markiewicz88@gmail.com>
2024-01-07 09:55:56 +0100

    Zadanie domowe 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var sex = "female";
+var name = "Ewa";
+var age = 18;
+
+if (sex == "female" && age < 30) 
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age == 30)
+{
+    Console.WriteLine("Ewa lat 30");
+}
+else if (sex == "male" && age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+else
+{
+    Console.WriteLine("Ktoś inny");
+}
